8af0cf897ca902408162befc33a8e7fdae905344
Francesco Boffa <fra.boffa@gmail.com>
2017-11-08 13:26:13 +0000

    Supporting jRuby as well

--- lib/jwa/cipher.rb
+++ lib/jwa/cipher.rb
@@ -2,9 +2,14 @@ module JWA
   # Helper to get OpenSSL cipher instance from a string.
   module Cipher
     class << self
+      # Ruby raises RuntimeError
+      # jRuby raises OpenSSL::Cipher::CipherError
+      # Also jRuby is sligthly more relaxed on cipher names, returning something
+      # even for aes-999-cbc, I don't know what it is and how it works.
+
       def for(cipher_name)
         OpenSSL::Cipher.new(cipher_name)
-      rescue RuntimeError
+      rescue RuntimeError, OpenSSL::Cipher::CipherError
         raise NotImplementedError, "The OpenSSL library provided by this Ruby version does not support #{cipher_name}."
       end
     end
--- spec/jwa/cipher_spec.rb
+++ spec/jwa/cipher_spec.rb
@@ -1,7 +1,7 @@
 describe JWA::Cipher do
   it 'raises if the cipher does not exist (or is not supported by this ruby)' do
     expect do
-      JWA::Cipher.for('aes-257-cbc')
+      JWA::Cipher.for('very-strange-cipher')
     end.to raise_error(NotImplementedError)
   end
 end
