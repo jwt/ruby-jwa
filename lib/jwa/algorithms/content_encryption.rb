1c960ef2e30059b7406dbdbb1220dedbf94621fd
Francesco Boffa <fra.boffa@gmail.com>
2017-11-11 15:52:51 +0000

    Helpers to get alg and enc from name

--- lib/jwa/algorithms/content_encryption.rb
+++ lib/jwa/algorithms/content_encryption.rb
@@ -5,3 +5,25 @@ require 'jwa/algorithms/content_encryption/a256_cbc_hs512'
 require 'jwa/algorithms/content_encryption/a128_gcm'
 require 'jwa/algorithms/content_encryption/a192_gcm'
 require 'jwa/algorithms/content_encryption/a256_gcm'
+
+module JWA
+  module Algorithms
+    module ContentEncryption
+      KNOWN_ENCS = {
+        'A128CBC-HS256' => A128CbcHs256,
+        'A192CBC-HS384' => A192CbcHs384,
+        'A256CBC-HS512' => A256CbcHs512,
+
+        'A128GCM' => A128Gcm,
+        'A192GCM' => A192Gcm,
+        'A256GCM' => A256Gcm
+      }.freeze
+
+      class << self
+        def for(name)
+          KNOWN_ENCS[name]
+        end
+      end
+    end
+  end
+end
--- lib/jwa/algorithms/key_management.rb
+++ lib/jwa/algorithms/key_management.rb
@@ -1,19 +1,56 @@
+require 'jwa/algorithms/key_management/rsa15'
+require 'jwa/algorithms/key_management/rsa_oaep'
+
 require 'jwa/algorithms/key_management/a128_kw'
 require 'jwa/algorithms/key_management/a192_kw'
 require 'jwa/algorithms/key_management/a256_kw'
-
-require 'jwa/algorithms/key_management/a128_gcm_kw'
-require 'jwa/algorithms/key_management/a192_gcm_kw'
-require 'jwa/algorithms/key_management/a256_gcm_kw'
 
 require 'jwa/algorithms/key_management/ecdh_es'
 require 'jwa/algorithms/key_management/ecdh_es_a128_kw'
 require 'jwa/algorithms/key_management/ecdh_es_a192_kw'
 require 'jwa/algorithms/key_management/ecdh_es_a256_kw'
 
-require 'jwa/algorithms/key_management/rsa15'
-require 'jwa/algorithms/key_management/rsa_oaep'
+require 'jwa/algorithms/key_management/a128_gcm_kw'
+require 'jwa/algorithms/key_management/a192_gcm_kw'
+require 'jwa/algorithms/key_management/a256_gcm_kw'
 
 require 'jwa/algorithms/key_management/pbes_hs256_a128_kw'
 require 'jwa/algorithms/key_management/pbes_hs384_a192_kw'
 require 'jwa/algorithms/key_management/pbes_hs512_a256_kw'
+
+module JWA
+  module Algorithms
+    module KeyManagement
+      KNOWN_ALGS = {
+        'RSA1_5' => Rsa15,
+        'RSA-OAEP' => RsaOaep,
+        'RSA-OAEP-256' => nil,
+
+        'A128KW' => A128Kw,
+        'A192KW' => A192Kw,
+        'A256KW' => A256Kw,
+
+        'dir' => nil,
+
+        'ECDH-ES' => EcdhEs,
+        'ECDH-ES+A128KW' => EcdhEs,
+        'ECDH-ES+A192KW' => EcdhEs,
+        'ECDH-ES+A256KW' => EcdhEs,
+
+        'A128GCMKW' => A128GcmKw,
+        'A192GCMKW' => A192GcmKw,
+        'A256GCMKW' => A256GcmKw,
+
+        'PBES2-HS256+A128KW' => Pbes2Hs256A128Kw,
+        'PBES2-HS384+A192KW' => Pbes2Hs384A192Kw,
+        'PBES2-HS512+A256KW' => Pbes2Hs512A256Kw
+      }.freeze
+
+      class << self
+        def for(name)
+          KNOWN_ALGS[name]
+        end
+      end
+    end
+  end
+end
